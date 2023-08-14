98b2389225de18bfa06ee8679662202be743c560
StanoSpodniak <stano28@gmail.com>
2023-08-14 18:25:17 +0200

    Added play game functionality

--- src/Main/Game/Game.js
+++ src/Main/Game/Game.js
@@ -3,6 +3,8 @@ import Problem from "./Problem";
 import "./game.css";
 
 const Game = ({ isNewGame, isGiveUp }) => {
+    const math = require('mathjs');
+
     const [firstProblem, setFirstProblem] = useState([]);
     const [secondProblem, setSecondProblem] = useState([]);
     const [thirdProblem, setThirdProblem] = useState([]);
@@ -11,6 +13,11 @@ const Game = ({ isNewGame, isGiveUp }) => {
     const [playNumbers, setPlayNumbers] = useState([]);
     const [clickedNumbers, setClickedNumbers] = useState([""]);
     const tileRefs = useRef([]);
+    const inventoryTileRefs = useRef([]);
+
+    const [submitButtonText, setSubmitButtonText] = useState("Submit");
+    const [disableSubmitButton, setDisableSubmitButton] = useState(true);
+    const [gameIsOver, setGameIsOver] = useState(false);
 
     useEffect(() => {
         setFirstProblem(Problem());
@@ -19,9 +26,9 @@ const Game = ({ isNewGame, isGiveUp }) => {
     },[]);
     
     useEffect(() => {
-        //console.log(firstProblem);
-        //console.log(secondProblem);
-        //console.log(thirdProblem);
+        console.log(firstProblem);
+        console.log(secondProblem);
+        console.log(thirdProblem);
 
         if (firstProblem && secondProblem && thirdProblem) {
             let numbers = [firstProblem.firstRndNmb];
@@ -51,6 +58,14 @@ const Game = ({ isNewGame, isGiveUp }) => {
     },[solution]);
 
     useEffect(() => {
+        if(clickedNumbers.includes("") || clickedNumbers.length !== playNumbers.length) {
+            setDisableSubmitButton(true);
+        } else {
+            setDisableSubmitButton(false);
+        }
+    }, [clickedNumbers, playNumbers]);
+
+    useEffect(() => {
         if(isNewGame) {
             handleNewGame();
         }
@@ -62,9 +77,96 @@ const Game = ({ isNewGame, isGiveUp }) => {
         }
     },[isGiveUp]);
 
-    const handleNewGame = () => {
+    const handleClick = (index) => {
+        const button = inventoryTileRefs.current[index];
+        button.disabled = true;
+
+        const currentPlayfield = [...clickedNumbers];
         for(let i = 0; i < 9; i++) {
-            setClickedNumbers("");
+            if(!currentPlayfield[i]) {
+                currentPlayfield[i] = button.innerText;
+                setClickedNumbers(currentPlayfield);
+                break;
+            }
+        }
+    }
+
+    const handleDelete = (index) => {
+        if(!gameIsOver) {
+            const currentPlayfield = [...clickedNumbers];
+        
+            for(let i = 0; i < playNumbers.length; i++) {
+                if(inventoryTileRefs.current[i].innerText === currentPlayfield[index] && inventoryTileRefs.current[i].disabled === true) {
+                    inventoryTileRefs.current[i].disabled = false;
+                    break;
+                }
+            }
+    
+            currentPlayfield[index] = "";
+            setClickedNumbers(currentPlayfield);
+        }
+    }
+
+    const handleSubmit = () => {
+        let canSubmit = true;
+
+        for(let i = 0; i < playNumbers.length; i++) {
+            if(clickedNumbers[i] === undefined) {
+                canSubmit = false;
+            }
+        }
+
+        if(canSubmit) {
+            if((math.evaluate(`${clickedNumbers[0]} ${firstProblem.mathSign} ${clickedNumbers[1]} ${firstProblem.secondMathSign} ${clickedNumbers[2]}`) === firstProblem.result) &&
+            (math.evaluate(`${clickedNumbers[3]} ${secondProblem.mathSign} ${clickedNumbers[4]} ${secondProblem.secondMathSign} ${clickedNumbers[5]}`) === secondProblem.result) &&  
+            (math.evaluate(`${clickedNumbers[6]} ${thirdProblem.mathSign} ${clickedNumbers[7]} ${thirdProblem.secondMathSign} ${clickedNumbers[8]}`) === thirdProblem.result)) {
+                setGameIsOver(true);
+                setSubmitButtonText("New Game");
+                for(let i = 0; i < 9; i++) {
+                    tileRefs.current[i].style.border = "2px solid #bdf5bd";
+                    tileRefs.current[i].style.color = "#189a18";
+                }
+            } else {
+                if(math.evaluate(`${clickedNumbers[0]} ${firstProblem.mathSign} ${clickedNumbers[1]} ${firstProblem.secondMathSign} ${clickedNumbers[2]}`) !== firstProblem.result) {
+                    changeStyleToWrong(0);
+                }
+    
+                if(math.evaluate(`${clickedNumbers[3]} ${secondProblem.mathSign} ${clickedNumbers[4]} ${secondProblem.secondMathSign} ${clickedNumbers[5]}`) !== secondProblem.result) {
+                    changeStyleToWrong(3);
+                }
+    
+                if(math.evaluate(`${clickedNumbers[6]} ${thirdProblem.mathSign} ${clickedNumbers[7]} ${thirdProblem.secondMathSign} ${clickedNumbers[8]}`) !== thirdProblem.result) {
+                    changeStyleToWrong(6);
+                }
+            }
+    
+            if(submitButtonText === "New Game") {
+                handleNewGame();
+            }
+        }
+    }
+
+    const changeStyleToWrong = (index) => {
+        for(let i = index; i < index + 3; i++) {
+            tileRefs.current[i].style.border = "2px solid #ff8080";
+            tileRefs.current[i].style.color = "#e60000";
+            setTimeout(() => {
+                tileRefs.current[i].style.border = "2px solid gainsboro";
+                tileRefs.current[i].style.color = "black";
+            }, "2000");
+        }
+    }
+
+    const handleNewGame = () => {
+        setGameIsOver(false);
+        setClickedNumbers([""]);
+        setPlayNumbers([]);
+        setSubmitButtonText("Submit");
+        setDisableSubmitButton(true);
+        
+        for(let i = 0; i < 9; i++) {
+            tileRefs.current[i].style.border = "2px solid gainsboro";
+            tileRefs.current[i].style.color = "black";
         }
 
         setFirstProblem(Problem());
@@ -73,8 +175,15 @@ const Game = ({ isNewGame, isGiveUp }) => {
     }
 
     const handleGiveUp = () => {
+        setGameIsOver(true);
+        setSubmitButtonText("New Game");
+
         for(let i = 0; i < 9; i++) {
             setClickedNumbers(solution);
+            inventoryTileRefs.current[i].disabled = true;
+
+            tileRefs.current[i].style.border = "2px solid #bdf5bd";
+            tileRefs.current[i].style.color = "#189a18";
         }
     }
 
@@ -83,19 +192,22 @@ const Game = ({ isNewGame, isGiveUp }) => {
             <div className="playfield">
                 <button 
                     className="tile"
-                    ref={ref => (tileRefs.current[0] = ref)}>
+                    ref={ref => (tileRefs.current[0] = ref)}
+                    onClick={() => handleDelete(0)}>
                     {clickedNumbers[0]}
                 </button>
                 <p className="item">{firstProblem.mathSign}</p>
                 <button 
                     className="tile"
-                    ref={ref => (tileRefs.current[1] = ref)}>
+                    ref={ref => (tileRefs.current[1] = ref)}
+                    onClick={() => handleDelete(1)}>
                     {clickedNumbers[1]}
                 </button>
                 <p className="item">{firstProblem.secondMathSign}</p>
                 <button 
                     className="tile"
-                    ref={ref => (tileRefs.current[2] = ref)}>
+                    ref={ref => (tileRefs.current[2] = ref)}
+                    onClick={() => handleDelete(2)}>
                     {clickedNumbers[2]}
                 </button>
                 <p className="item">=</p>
@@ -104,19 +216,22 @@ const Game = ({ isNewGame, isGiveUp }) => {
             <div className="playfield">
                 <button 
                     className="tile"
-                    ref={ref => (tileRefs.current[3] = ref)}>
+                    ref={ref => (tileRefs.current[3] = ref)}
+                    onClick={() => handleDelete(3)}>
                     {clickedNumbers[3]}
                 </button>
                 <p className="item">{secondProblem.mathSign}</p>
                 <button 
                     className="tile"
-                    ref={ref => (tileRefs.current[4] = ref)}>
+                    ref={ref => (tileRefs.current[4] = ref)}
+                    onClick={() => handleDelete(4)}>
                     {clickedNumbers[4]}
                 </button>
                 <p className="item">{secondProblem.secondMathSign}</p>
                 <button 
                     className="tile"
-                    ref={ref => (tileRefs.current[5] = ref)}>
+                    ref={ref => (tileRefs.current[5] = ref)}
+                    onClick={() => handleDelete(5)}>
                     {clickedNumbers[5]}
                 </button>
                 <p className="item">=</p>
@@ -125,29 +240,37 @@ const Game = ({ isNewGame, isGiveUp }) => {
             <div className="playfield">
                 <button 
                     className="tile"
-                    ref={ref => (tileRefs.current[6] = ref)}>
+                    ref={ref => (tileRefs.current[6] = ref)}
+                    onClick={() => handleDelete(6)}>
                     {clickedNumbers[6]}
                 </button>
                 <p className="item">{thirdProblem.mathSign}</p>
                 <button 
                     className="tile"
-                    ref={ref => (tileRefs.current[7] = ref)}>
+                    ref={ref => (tileRefs.current[7] = ref)}
+                    onClick={() => handleDelete(7)}>
                     {clickedNumbers[7]}
                 </button>
                 <p className="item">{thirdProblem.secondMathSign}</p>
                 <button 
                     className="tile"
-                    ref={ref => (tileRefs.current[8] = ref)}>
+                    ref={ref => (tileRefs.current[8] = ref)}
+                    onClick={() => handleDelete(8)}>
                     {clickedNumbers[8]}
                 </button>
                 <p className="item">=</p>
                 <p className="item result">{thirdProblem.result}</p>
+            </div>
+            <div className="submit-field">
+                <button id="submit-button" onClick={handleSubmit} disabled={disableSubmitButton}>{submitButtonText}</button>
             </div>
             <div id="inventory">
                 {playNumbers.map((number, i) => (
                     <button
                         key={i}
                         className="tile"
+                        ref={ref => (inventoryTileRefs.current[i] = ref)} 
+                        onClick={() => handleClick(i)}
                         >{number}
                     </button>
                 ))}
